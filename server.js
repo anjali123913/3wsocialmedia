cdb122bf3323c588bc30ab2d7e5e8b10515b2be6
anjali123913 <anjliverma95062@gmail.com>
2025-11-02 22:59:15 +0530

    udated

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000", // React local dev
+      "http://localhost:5173", // Vite local dev
+      "https://roaring-khapse-383869.netlify.app", // your deployed frontend
+    ],
+    credentials: true, // if using cookies/auth headers
+  })
+);
+
 app.use(express.json({ limit: "10mb" })); // allow image base64 prototypes
 
 connectDB(process.env.MONGO_URI);
